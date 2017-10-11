9b04c984ee9d3a16a7aab7b88678d577aeea2f32
satish <satheesh.narsingoju@yahoo.com>
2017-10-11 16:14:42 +0530

    dwesd

--- one.c
+++ one.c
@@ -1,16 +1,8 @@
 #include<stdio.h>
 int main()
 {
-int ddd;
-int cc;
-printf("git stash is working");
-int a;
-<<<<<<< HEAD
-  int b
-=======
   int b;
 int c;
->>>>>>> new_branch
 printf("added in the web");
 printf("first line code\n");
 printf("2nd line code\n");
